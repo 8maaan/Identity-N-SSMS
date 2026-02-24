2cbef8d0e80472b4fb5cd590e2c786d9d763c2c6
8maaan <johnrandolf696969@gmail.com>
2026-02-24 19:04:57 +0800

    Add project files.

---
+++ Demo/Data/AuthDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AuthDbContext()
+        {
+        }
+    }
+}
